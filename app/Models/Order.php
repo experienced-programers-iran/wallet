40dfe9b595c4e8e3df4f21422fb94ddc92b93fd0
Saeed Nikookalam <saeednikookalam@gmail.com>
2023-04-07 03:17:52 +0330

    feature: create order model and migration

---
+++ app/Models/Order.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Order extends Model
+{
+    use SoftDeletes;
+
+    const ID = 'id';
+}
