d74ac2be872d5c6d18b76f26765bd9e592e22572
Saeed Nikookalam <saeednikookalam@gmail.com>
2023-04-07 02:54:32 +0330

    feature: implement query log for better debug

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +25,20 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+
+        // Log all query in a request
+        if (app()->environment('local')) {
+            DB::listen(function ($query) {
+                $array = array_map(function ($item) {
+                    return is_object($item) || is_array($item) ? json_encode($item) : $item;
+                }, collect($query->bindings)->toArray());
+
+                File::append(
+                    storage_path('/logs/query.log'),
+                    $query->sql .
+                    ' [' . implode(', ', $array) . '] ' . 'time:' . $query->time . PHP_EOL
+                );
+            });
+        }
     }
 }
